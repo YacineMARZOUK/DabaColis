eeba4582f997388d72d0d09986c815ead07fc79b
YacineMARZOUK <yacinemarzou@gmail.com>
2025-11-27 16:44:14 +0100

    create the ColisResponse & ColisRequest dtos

---
+++ src/main/java/logistics/dto/auth/colis/ColisResponse.java
@@ -0,0 +1,29 @@
+package logistics.dto.auth.colis;
+
+import logistics.enums.ColisStatus;
+import logistics.enums.ColisType;
+import lombok.Builder;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+public class ColisResponse {
+    private String id;
+    private ColisType type;
+    private double poids;
+    private String adresseDestination;
+    private ColisStatus statut;
+
+
+    private UserResponse transporteur;
+
+
+    private String instructionsManutention;
+    private Double temperatureMin;
+    private Double temperatureMax;
+
+    private LocalDateTime createdDate;
+    private LocalDateTime lastModifiedDate;
+}
