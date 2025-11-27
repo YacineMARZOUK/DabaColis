ae81228761a46fb738bf01e7763c3e5f84848d26
YacineMARZOUK <yacinemarzou@gmail.com>
2025-11-27 16:08:38 +0100

    creete FrigoColis entity

---
+++ src/main/java/logistics/entity/FrigoColis.java
@@ -0,0 +1,17 @@
+package logistics.entity;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+import org.springframework.data.annotation.TypeAlias;
+
+@Data
+@SuperBuilder
+@NoArgsConstructor
+@EqualsAndHashCode(callSuper = true)
+@TypeAlias("FRIGO")
+public class FrigoColis extends Colis{
+    private Double temperatureMin;
+    private Double temperatureMax;
+}
