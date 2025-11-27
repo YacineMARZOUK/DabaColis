51a03fd15ddbef0fe33e228cbb15dd9f93a92db8
YacineMARZOUK <yacinemarzou@gmail.com>
2025-11-27 16:04:49 +0100

    creete theFragileColis entity

---
+++ src/main/java/logistics/entity/FragileColis.java
@@ -0,0 +1,16 @@
+package logistics.entity;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+import org.springframework.data.annotation.TypeAlias;
+
+@Data
+@SuperBuilder
+@NoArgsConstructor
+@EqualsAndHashCode(callSuper = true)
+@TypeAlias("FRAGILE")
+public class FragileColis extends Colis{
+    private String instructionsManutention;
+}
