b2bfffd93dd0e362dcfac5b240bc64447c18ec08
YacineMARZOUK <yacinemarzou@gmail.com>
2025-11-28 10:19:23 +0100

    create UserRepository & ColisRepository

---
+++ src/main/java/logistics/repository/UserRepository.java
@@ -0,0 +1,26 @@
+package logistics.repository;
+
+import logistics.entity.User;
+import logistics.enums.Speciality;
+import logistics.enums.UserRole;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends MongoRepository<User, String> {
+
+    Optional<User> findByLogin(String login);
+
+
+    Page<User> findByRole(UserRole role, Pageable pageable);
+
+
+    Page<User> findByRoleAndSpeciality(UserRole role, Speciality speciality, Pageable pageable);
+
+
+    boolean existsByLogin(String login);
+}
